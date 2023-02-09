3d6b54b88d53935315bf7052e93c92b1310cf380
Sandi Sopian <shunasand73@gmail.com>
2023-02-09 15:28:03 +0700

    feat: Adds @actions/core

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-const person = process.env.INPUT_PERSON;
+// without using library @actions/core
+// const person = process.env.INPUT_PERSON;
+
+// using library @actions/core
+const core = require("@actions/core");
+const person = core.getInput("person");
 
 console.log(`Hello ${person}!`);
